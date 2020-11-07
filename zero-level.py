897865e805d23a46f4c3a71d1425f3fda7a8f16e
firdausraginda <ragindafirdaus01@gmail.com>
2020-11-07 23:37:26 +0700

    initial commit

---
+++ zero-level.py
@@ -0,0 +1,32 @@
+# USER INPUT
+# user_says = input("Please enter the string you want to print: ")
+# print(user_says)
+
+# -----------------------------
+
+# VARIABLES
+# 1. should start w/ a letter (can't start with a number)
+# 2. can't include spaces 
+# 3. can't use symbols other than underscore (_)
+# 4. Hyphens are not permitted
+# 5. variable names are case sensitive
+
+# how to assign multiple variables
+# a = b = c = 10
+# a, b, c = 1, 2, 3
+# print(c)
+
+# check pointer
+# a = 10
+# b = 10
+# var a & b have the same location on memory bc both are pointing the same value
+
+# python keyword that can't be variable: https://docs.python.org/3/reference/lexical_analysis.html#keywords
+
+# -----------------------------
+
+# DATA TYPE
+# strings, numbers, booleans, lists, sets, frozensets, tuples, ranges, dictionaries, None
+
+# mutability = data type that can be modified after creation (lists, dictionaries, & sets)
+# immutability = data type that CANNOT be modified after creation (strings, numbers, tuples, & frozensets)
