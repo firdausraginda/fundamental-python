35dadeefd5600302615973a0fbda428b09e2fd8d
firdausraginda <ragindafirdaus01@gmail.com>
2020-11-28 15:23:29 +0700

    add ranges

---
+++ zero-level-#7-ranges.py
@@ -0,0 +1,19 @@
+# declare range
+# r = range(10)
+# print(r)
+
+# find item in index
+# r = range(10)
+# print(r.index(9))
+
+# convert range to list
+# r = range(10)
+# listr = list(r)
+# print(listr)
+
+# slicing
+# r = range(10)
+# listr = list(r)
+# print(r[0])
+# print(r[-1])
+# print(listr[2:5])
