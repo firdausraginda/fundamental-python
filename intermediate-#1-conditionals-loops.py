ddff6effc39830a5e7c9ffff03d243e743659c2b
firdausraginda <ragindafirdaus01@gmail.com>
2020-11-29 18:19:00 +0700

    add conditional loops

---
+++ intermediate-#1-conditionals-loops.py
@@ -0,0 +1,46 @@
+# RULE
+# python uses indentation to define code blocks (if, for, while), function & clases
+# uses indentation means that white spaces (4 spaces or a tab key) are used as delimiters for code blocks
+# after every if, for, while statement, or function or class definition, must use a colon
+
+# ----------------------------------------------------------
+
+# IF ELSE STATEMENT
+
+# x = 5
+
+# if x == 5 and type(x) is int:
+#     print("x equals 5 & is int")
+#     print(x)
+# elif x == 10 and type(x) is int:
+#     print("x equals 5 & is int")
+
+# ----------------------------------------------------------
+
+# LOOPS
+
+# example loop
+# vendors = ["Cisco", "HP", "Nortel", "Avaya", "Juniper"]
+
+# for each_vendor in vendors:
+#     print(each_vendor)
+# else:
+#     print("The end of list has been reached")
+
+# loop with range
+# r = range(10)
+
+# for i in r:
+#     print(i * 2)
+
+# loop with range and length of the list
+# vendors = ["Cisco", "HP", "Nortel", "Avaya", "Juniper"]
+
+# for el_index in range(len(vendors)):
+#     print(vendors[el_index])
+
+# loop with enumerate
+# vendors = ["Cisco", "HP", "Nortel", "Avaya", "Juniper"]
+
+# for idx, el in enumerate(vendors):
+#     print(idx, el)
