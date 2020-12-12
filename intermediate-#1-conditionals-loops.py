a08ff86bfa1fa2ed0eab86e1e5f79da1bb8154e1
firdausraginda <ragindafirdaus01@gmail.com>
2020-12-12 15:27:39 +0700

    finish conditionals loops

--- intermediate-#1-conditionals-loops.py
+++ intermediate-#1-conditionals-loops.py
@@ -52,10 +52,135 @@
 # while loops will continue looping until the value become false
 
 # example while loop
-x = 1
+# x = 1
 
-while x <= 10:
-    print(x)
-    x = x + 1
-else:
-    print("x is now > 10")
+# while x <= 10:
+#     print(x)
+#     x = x + 1
+# else:
+#     print("x is now > 10")
+
+# ----------------------------------------------------------
+
+# NESTED IF STATEMENT & FOR LOOPS
+# inner if statement will be executed if the outer statement is true
+
+# example of nested if statement
+# x = "Cisco"
+
+# if "i" in x:
+#     if len(x) > 3:
+#         print(x, len(x))
+
+# example of nested for loops
+# list1 = [4,5,6]
+# list2 = [10,20,30]
+
+# for i in list1:
+#     for j in list2:
+#         print(i*j)
+#     print('---',i,'---')
+
+# example of nested if statement inside for loops
+# for number in range(10):
+#     if 5 <= number <= 9:
+#         print(number)
+
+# ----------------------------------------------------------
+
+# BREAK STATEMENT
+
+# break will ignore the print function & completely quit the for loops
+# for number in range(10):
+#     if number == 7:
+#         break
+#     print(number)
+
+# break only terminates the execution of the innermost loop, not all the loops
+# list1 = [4,5,6]
+# list2 = [10,20,30]
+
+# for i in list1:
+#     for j in list2:
+#         if j == 20:
+#             break
+#         print(i*j)
+#     print("outside the nested loop")
+
+# ----------------------------------------------------------
+
+# CONTINUE STATEMENT
+
+# continue statement will not stop the whole iteration of the innermost for loop, instead, it only stops the current iteration
+# list1 = [4,5,6]
+# list2 = [10,20,30]
+
+# for i in list1:
+#     for j in list2:
+#         if j == 20:
+#             continue
+#         print(i*j)
+#     print("outside the nested loop")
+
+# ----------------------------------------------------------
+
+# PASS STATEMENT
+
+# pass statement is equal of do nothing
+# pass statement is just a placeholder for whenever you want to leave the addition of a piece of code for later
+
+# for i in range(10):
+#     pass
+
+# ----------------------------------------------------------
+
+# ERROR IN PYTHON
+# syntax error: occurs when you don'f follow python's syntax (forget to add colon or indentation is not proper)
+# exceptions: exceptions are raised during the execution of the program interrupting the normal flow of the application
+
+# ----------------------------------------------------------
+
+# EXCEPTIONS
+
+# correctly specify the error that may occurs
+# for i in range(5):
+#     try:
+#         print(i / 0)
+#     except ZeroDivisionError as e:
+#         print(e, "Division by 0 is not allowed, sorry!")
+
+# wrong specify the error that may occurs, will only return the plain old exception message (default error message)
+# for i in range(5):
+#     try:
+#         print(i / 0)
+#     except NameError as e:
+#         print(e, "name error, sorry!")
+
+# specify multiple exceptions
+# for i in range(5):
+#     try:
+#         print(i / 0)
+#     except ZeroDivisionError as e:
+#         print(e, "division by zero is not allowed, sorry!")
+#     except NameError as e:
+#         print(e, "name error, sorry!")
+#     except ValueError as e:
+#         print(e, "value error, sorry!")
+
+# catch all possible exceptions that my occurs
+# for i in range(5):
+#     try:
+#         print(i / 0)
+#     except:
+#         print("error detected, sorry!")
+#     else:
+#         print("no exception detected")
+
+# finally clause would still be executed regardless exception occurs or not
+# for i in range(5):
+#     try:
+#         print(i / 0)
+#     except:
+#         print("error detected, sorry!")
+#     finally:
+#         print("i don't care, i'm getting printed anyway")
