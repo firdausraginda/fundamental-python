2be84d5504a02e56b2fa7992bbc1237daacd9222
firdausraginda <ragindafirdaus01@gmail.com>
2020-12-20 14:12:25 +0700

    finish file operation all sections

--- intermediate-#4-file-operations.py
+++ intermediate-#4-file-operations.py
@@ -24,7 +24,7 @@
 
 # ----------------------------------------------------------
 
-# print only chart with 'a' char in the begining
+# print data with 'a' char in the begining
 
 # myfile = open("./routers.txt", "r")
 # myfile.seek(0)
@@ -39,8 +39,63 @@
 
 # python has a set of special characters also called escape sequences: '\n', '\t', '\U'
 
-# the solution is to use double backslash: f = open('C:\\Users\\todo\\Desktop\\new\\test.txt', 'r')
-# or can use r in front of the path: f = open(r'D:\Users\todo\file1.py', 'w')
+# the solution is to use double backslash: f = open("C:\\Users\\todo\\Desktop\\new\\test.txt", "r")
+# or can use r in front of the path: f = open(r"D:\Users\todo\file1.py", "w")
 
 # ----------------------------------------------------------
 
+# WRITING TO FILE
+
+# newfile = open(r"./newfile.txt", "w") # write a file if not exist, replace if exist
+
+# newfile.write('''I like Python! 
+# Do you?
+# ''') # write something inside the file
+
+# newfile.close() # close write mode
+
+# newfile = open(r"./newfile.txt", "r") # read file
+# print(newfile.read())
+
+# ----------------------------------------------------------
+
+# APPENDING TO FILE
+
+# newfile = open(r"./newfile.txt", "w") # write a file if not exist, replace if exist
+# newfile.writelines(["Cisco", "Juniper", "HP"]) # write something in list
+
+# newfile = open(r"./newfile.txt", "a") # append file
+# newfile.write("this string was appended")
+# newfile.close()
+
+# ----------------------------------------------------------
+
+# CLOSE AUTOMATICALLY USING WITH .. AS ..
+
+# with open(r"./newfile.txt", "w") as f:
+#     f.write("Hello Python!!!")
+
+# res = open(r"./newfile.txt", "r")
+# print(res.read()) # read file
+# print(f.closed) # check whether it is closed or not
+
+# ----------------------------------------------------------
+
+# DELETING FILE CONTENTS
+
+# read the length of the file content
+# f = open("./text.txt")
+# print(f.read())
+# f.seek(0)
+# print(len(f.read())) # print length of file content
+# f.close()
+
+# delete partial of the file content
+# f = open("./text.txt", "r+") # open the file for reading & writing simultanously
+# f.truncate(10) # delete all except the first 10 characters
+# f.close()
+
+# delete all the file content
+# f = open("./text.txt", "r+") # open the file for reading & writing simultanously
+# f.truncate() # delete all file content
+# f.close()
