b5517cd585f5b94b676f28f32635f67868b5499a
firdausraginda <ragindafirdaus01@gmail.com>
2020-12-25 14:48:30 +0700

    itertools done

--- intermediate-#7-other-advance-topics.py
+++ intermediate-#7-other-advance-topics.py
@@ -48,7 +48,6 @@
 #             result = x * y
 #             list_xy.append(result)
 #     return list_xy + mylist
-
 # print(myfunc([100,101,102]))
 
 # LAMBDA FUNCTION
@@ -112,3 +111,45 @@
 # print(next(my_object))
 # print(next(my_object))
 # print(next(my_object))
+
+# ----------------------------------------------------------
+
+# ITERTOOLS: CHAIN(), COUNT(), CYCLE(), FILTERFALSE(), ISLICE()
+# chain(): combine multiple lists into 1 sequence
+# count(): returns an iterator that generates consecutive integers until you stop it
+# cycle(): returns an iterator that simply repeats the value given as an argument infinitely
+# filterfalse(): returns the elements for which the function you provide as an argument returns false
+# islice(): works the same as string slices
+
+# from itertools import *
+
+# CHAIN FUNCTION
+# list1 = [1,2,3,'a','b','c']
+# list2 = [101,102,103,'X','Y']
+
+# print(list(chain(list1, list2))) # convert chain list1 & list2 into list
+
+# for i in chain(list1, list2): # for loop with list1 & list2 as the iterator
+#     print(i)
+
+# COUNT FUNCTION
+# for i in count(10, 2.5):
+#     if i <= 50:
+#         print(i)
+#     else:
+#         break
+
+# CYCLE FUNCTION
+# a = range(11, 16)
+# for i in cycle(a):
+#     print(i)
+
+# FILTERFALSE FUNCTION
+# a = list(filterfalse(lambda x: x < 5, [1,2,3,4,5,6,7]))
+# print(a)
+
+# ISLICE FUNCTION
+# b = islice(range(10), 2, 9, 2)
+# print(list(b))
+
+# ----------------------------------------------------------
